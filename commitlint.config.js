85846c8507281a8750e807ab800dc6332d7f6263
Philip Polkovnikov <polkovnikov.ph@gmail.com>
2021-07-06 05:28:28 +0300

    build: prepare for publication

---
+++ commitlint.config.js
@@ -0,0 +1,8 @@
+// import type {UserConfig} from '@commitlint/types';
+
+// const config: UserConfig = {
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+};
+
+// export = config;
